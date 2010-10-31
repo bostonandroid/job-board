e152451bc9ee8614dbbcb34dbea54f042c870699
Mike Burns <mburns@thoughtbot.com>
2010-10-31 15:05:22 -0400

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+set :application, "bandjob"
+
+set :repository,  "git@github.com:bostonandroid/job-board.git"
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+set :deploy_to, "/usr/home/mike/#{application}"
+set :user, "mike"
+
+role :app, "mike-burns.com"
+role :web, "mike-burns.com"
+role :db,  "mike-burns.com", :primary => true
+
+namespace :deploy do
+  desc "Default deploy - updated to run migrations"
+  task :default do
+    set :migrate_target, :latest
+    update_code
+    symlink
+    migrate
+    restart
+  end
+
+  desc "Run this after every successful deployment"
+  task :after_default do
+    cleanup
+  end
+
+  %w(start stop restart).each do |action|
+    desc "#{action} the Thin server"
+    task action.to_sym, :roles => :app do
+      run "thin #{action} -c #{deploy_to}/current -C /usr/local/etc/thin/#{application}.conf"
+    end
+  end
+end
+
